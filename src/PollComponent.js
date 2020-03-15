6745504f0502ef60dcbb28ce8f87f017f8df4195
Bradley-B <brad.boxer1@gmail.com>
2020-03-15 14:08:11 -0400

    Update Poll Results Periodically
    Performs a new GET request every 5 seconds to update the poll results.

--- src/PollComponent.js
+++ src/PollComponent.js
@@ -9,8 +9,38 @@ export default class PollComponent extends React.Component {
         this.state = {isloading: true, poll_jsx: loading_jsx};
     }
 
+    processQuestionToResultJsx(question) {
+        let positive_votes = question.positivevotes;
+        let negative_votes = question.negativevotes;
+        let vote_percent = positive_votes/(positive_votes+negative_votes);
+        if(isNaN(vote_percent)) {
+            vote_percent = 1;
+        }
+        return  <p key={question.questionid}>{question.name + ": " + (vote_percent*100).toFixed(2) + "%"}</p>;
+    }
+
+    updateVotes() {
+        fetch("/api/get/"+this.props.id).then((response) => {
+            response.json().then((result) => {
+                if(result.exists === false) {
+                    return;
+                }
+
+                let result_jsx = [];
+                for(const [index, value] of result.questions.entries()) {
+                    result_jsx.push(
+                        this.processQuestionToResultJsx(value)
+                    );
+                }
+                this.setState({result_jsx: result_jsx});
+            });
+        });
+    }
+
     componentDidMount() {
         //get poll information here - ajax call or socket connection
+
+        this.interval = setInterval(()=>{this.updateVotes()}, 5000);
 
         fetch("/api/get/"+this.props.id).then((response) => {
             response.json().then((result) => {
@@ -26,20 +56,18 @@ export default class PollComponent extends React.Component {
                     poll_jsx.push(
                         <PollQuestionComponent key={value.questionid} info={value} authorname={this.props.authorname}/>
                     );
-                    let positive_votes = value.positivevotes;
-                    let negative_votes = value.negativevotes;
-                    let vote_percent = positive_votes/(positive_votes+negative_votes);
-                    if(isNaN(vote_percent)) {
-                        vote_percent = 1;
-                    }
                     result_jsx.push(
-                        <p key={value.questionid}>{value.name + ": " + (vote_percent*100).toFixed(2) + "%"}</p>
+                        this.processQuestionToResultJsx(value)
                     );
                 }
                 this.setState({isloading: false, exists: true, poll_jsx: poll_jsx, pollname: result.name, result_jsx: result_jsx});
             });
         });
 
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     render() {
