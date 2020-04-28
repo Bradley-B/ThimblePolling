21a1be8dd32ed82f59e7e05cc248c4fe5d3d79ca
Bradley-B <brad.boxer1@gmail.com>
2020-04-27 21:18:23 -0400

    Add And Remove Poll Items

--- client/src/CreatePoll.js
+++ client/src/CreatePoll.js
@@ -3,7 +3,25 @@ import React from "react";
 export default class CreatePoll extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {items: [this.pollItemObject(true, '')]};
+        this.add = this.add.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
+
+    onTextChange = changedItemId => e => {
+        e.preventDefault();
+        this.setState({items: this.state.items.map((item, id) => {
+            if(changedItemId !== id) return item;
+            return this.pollItemObject(item.checked, e.target.value);
+        })});
+    };
+
+    onCheckChange = changedItemId => e => {
+        this.setState({items: this.state.items.map((item, id) => {
+                if(changedItemId !== id) return item;
+                return this.pollItemObject(e.target.checked, item.value);
+        })});
+    };
 
     onSubmit(e) {
         alert("submitted");
@@ -11,30 +29,37 @@ export default class CreatePoll extends React.Component {
     }
 
     add(e) {
-        alert("add");
         e.preventDefault();
+        this.setState({items: this.state.items.concat([this.pollItemObject(true, '')])});
     }
 
-    remove(e) {
-        alert("remove");
+    remove = removedItemId => e => {
         e.preventDefault();
+        if(removedItemId < 1) return;
+        this.setState({items: this.state.items.filter((item, id) => removedItemId !== id)});
+    };
+
+    pollItemObject(checked, value) {
+        return {checked: checked, value: value};
     }
 
-    pollItem() {
-        return <div className="create-poll-item">
-            <input defaultChecked={true} type={"checkbox"}/>
-            <input required={true} type={"text"} placeholder="Option"/>
+    pollItemJsx(pollItemObject, id) {
+        return <div key={id} className="create-poll-item">
+            <input onChange={this.onCheckChange(id)} defaultChecked={true} type={"checkbox"} value={pollItemObject.checked}/>
+            <input onChange={this.onTextChange(id)} required={true} type={"text"} placeholder="enter option here" value={pollItemObject.value}/>
         </div>;
     }
 
     render() {
         return <form className={"create-poll-form"} onSubmit={this.onSubmit}>
             <div className={"create-poll-items"}>
-                {[this.pollItem(), this.pollItem(), this.pollItem(), this.pollItem(), this.pollItem()]}
+                {this.state.items.map((item, id) => {
+                    return this.pollItemJsx(item, id);
+                })}
             </div>
             <div className={"create-poll-buttons"}>
                 <button onClick={this.add} id="add">Add Option</button>
-                <button onClick={this.remove} id="remove">Remove Option</button>
+                <button onClick={this.remove(this.state.items.length - 1)} id="remove">Remove Option</button>
                 <input id="submit" type={"submit"}/>
             </div>
         </form>
