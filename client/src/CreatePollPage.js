16db9e90fc6e2c2c0aa1fed6ff6c6d2cacd72aff
Bradley-B <brad.boxer1@gmail.com>
2020-04-30 21:57:15 -0400

    Control Create Poll Sidebar Component
    Keep track of the state of the information entered in the sidebar.

--- client/src/CreatePollPage.js
+++ client/src/CreatePollPage.js
@@ -7,11 +7,17 @@ export default class CreatePollPage extends React.Component {
     constructor(props) {
         super(props);
         this.onBubbleStateChange = this.onBubbleStateChange.bind(this);
-        this.state = {pollItems: [{checked: true, value: ''}]};
+        this.onSidebarStateChange = this.onSidebarStateChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {requireLogin: true, pollName: '', pollItems: [{checked: true, value: ''}]};
     }
 
-    onSubmit(e) {
-        alert("submitted");
+    onSubmit() {
+        alert(JSON.stringify(this.state));
+    }
+
+    onSidebarStateChange(state) {
+        this.setState(state);
     }
 
     onBubbleStateChange(pollItems) {
@@ -23,7 +29,7 @@ export default class CreatePollPage extends React.Component {
             <div className={"title-container"}><h1 className={"title"}>Create Poll</h1></div>
             <div><Confetti/></div>
             <div id={"talk-bubble"}><CreatePollBubble onSubmit={this.onSubmit} onBubbleStateChange={this.onBubbleStateChange} pollItems={this.state.pollItems}/></div>
-            <div id={"sidebar"}><CreatePollSidebar /></div>
+            <div id={"sidebar"}><CreatePollSidebar onSidebarStateChange={this.onSidebarStateChange} pollName={this.state.pollName} requireLogin={this.state.requireLogin}/></div>
         </>;
     }
 }
--- client/src/CreatePollSidebar.js
+++ client/src/CreatePollSidebar.js
@@ -3,15 +3,33 @@ import React from 'react';
 export default class CreatePollSidebar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {name: ''};
+        this.onRequireLoginChange = this.onRequireLoginChange.bind(this);
+        this.onNameChange = this.onNameChange.bind(this);
+    }
+
+    onRequireLoginChange(e) {
+        let name = e.target.name;
+        let value = e.target.checked;
+        this.props.onSidebarStateChange({[name]: value});
+    }
+
+    onNameChange(e) {
+        e.preventDefault();
+        let {name, value} = e.target;
+        this.props.onSidebarStateChange({[name]: value});
     }
 
     render() {
         return <div className={"sidebar-content"}>
             <h2>Poll Preferences</h2>
             <form className={"create-poll-sidebar-form"}>
-                <label htmlFor="name">Name</label><input required id="name" type="text" placeholder="enter poll name here"/>
-                <label>Require Login</label><input type="checkbox"/>
+                <label htmlFor="name">Name</label>
+                <input onChange={this.onNameChange} value={this.props.pollName} required
+                       name="pollName" id="pollName" type="text" placeholder="enter poll name here"/>
+
+                <label htmlFor="requireLogin">Require Login</label>
+                <input onChange={this.onRequireLoginChange} value={this.props.requireLogin}
+                       name="requireLogin" id="requireLogin" defaultChecked={true} type="checkbox"/>
             </form>
         </div>;
     }
