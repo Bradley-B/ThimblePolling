8b3f073b07b3eb336c27747208b74a136302faac
Bradley-B <brad.boxer1@gmail.com>
2020-05-05 17:48:32 -0400

    Update Upload Script
    Zip up the build folder so it doesn't have to be done manually.

--- upload.sh
+++ upload.sh
@@ -1,2 +1,6 @@
+rm client/build.zip
+cd client
+zip -r build.zip build
+cd ..
 scp -i ~/.ssh/id_rsa_webserver.pub server/server.js server/tools.js server/database.js client/build.zip bradxdut@webserver:/home/bradxdut/bettervoting
 ssh bradxdut@webserver 'cd /home/bradxdut/bettervoting; rm -r build; unzip build.zip'
